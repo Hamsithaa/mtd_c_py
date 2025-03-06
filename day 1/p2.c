525388380317e90112d93ccf86c79bbf1b3b0674
hamsithaa <hamsithaa08@gmail.com>
2025-03-06 15:33:40 +0530

    codes

--- day 1/p2.c
+++ day 1/p2.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int num1 = 0;
-    long int num2 = 0;
-    long long int num3 = 0;
-    double d = 0.0;
-    float f = 0.0f;
-    printf("%d %d %d %d %d", sizeof(2),sizeof(0.25697),sizeof(num1),sizeof())
+    int i = -1;
+    
+    return 0;
 }
