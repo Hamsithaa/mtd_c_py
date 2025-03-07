a1088da3805e51ead976d12f4ef09093f65b8968
hamsithaa <hamsithaa08@gmail.com>
2025-03-07 12:48:16 +0530

    codes

--- day 2/p4.c
+++ day 2/p4.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    
+    int num=0;
+    puts("Enter a number to check if it is a perfect square");
+    scanf("%d",&num);
+    int root =floor(sqrt(num));
+    if(root*root==num)
+    printf("%d is a perfect square",num);
+    else 
+    printf("%d is not a perfect square",num);
 
     return 0;
 }
